7b4e6d01ce32dc36ac82465dd1a01f78b6332c24
NostemLucas <102114238+NostemLucas@users.noreply.github.com>
2025-03-09 16:42:36 -0400

    feat(add fonts): fonts css

---
+++ components/buttons/Button.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+	import { tv } from 'tailwind-variants';
+	import { twMerge } from 'tailwind-merge';
+	import { defineProps } from 'vue';
+
+	// Define las props
+	const props = defineProps<{
+		variant?: 'primary' | 'secondary';
+		size?: 'sm' | 'lg';
+		text: string;
+		class?: string;
+	}>();
+
+	const button = tv({
+		base: 'px-4 py-2 font-bold',
+		variants: {
+			variant: {
+				primary: 'bg-blue-500 text-white',
+				secondary: 'bg-gray-500 text-black',
+			},
+			size: {
+				sm: 'text-sm',
+				lg: 'text-lg',
+			},
+		},
+		defaultVariants: {
+			variant: 'primary',
+			size: 'sm',
+		},
+	});
+</script>
+
+<template>
+	<button
+		:class="
+			twMerge(button({ variant: props.variant, size: props.size }), props.class)
+		"
+	>
+		{{ props.text }}
+	</button>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-	<div class="container bg-amber-600">sdsdsd</div>
+	<div class="container bg-amber-600">
+		<Button class="font-roboto" variant="primary" text="primary" />
+		<Button class="font-horsham" variant="secondary" text="secondary" />
+		<Button variant="secondary" text="secondary" />
+	</div>
 </template>
+<script setup>
+	import Button from '@/components/buttons/Button.vue';
+</script>
