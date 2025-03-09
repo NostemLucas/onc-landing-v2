29acb10dc732b5bf11fda896a3ae42ac2a512cae
NostemLucas <102114238+NostemLucas@users.noreply.github.com>
2025-03-09 15:41:58 -0400

    feat(eslint configuration): eslint Module

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+		<NuxtRouteAnnouncer />
+		<NuxtWelcome />
+		{{ estado.sdsd }}
+	</div>
 </template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+	import NuxtRouteAnnouncer from './components/NuxtRoduteAnnouncer.vue';
+	import NuxtWelcome from './components/NuxwsdsdtWelcome.vue';
+</script>
---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintPluginVue from 'eslint-plugin-vue';
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(
+	{
+		plugins: {
+			prettier: prettierPlugin,
+			vue: eslintPluginVue,
+		},
+		rules: {
+			'prettier/prettier': [
+				'error',
+				{
+					endOfLine: 'auto',
+				},
+			],
+		},
+	},
+
+	{
+		files: ['**/*.ts', '**/*.vue', '**/*.js'],
+		rules: {
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-unused-vars': 'off',
+
+			'vue/multi-word-component-names': 'off',
+			'vue/html-self-closing': 'off',
+
+			'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+			'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		},
+	}
+);
