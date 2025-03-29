5b4eed14f19f63d2b61a1d03c1aced9a1075b36e
Limberg Edgar Montes Tancara <nostemlucas@gmail.com>
2025-03-29 13:23:54 -0400

    fix

--- mocks/doctors.ts
+++ mocks/doctors.ts
@@ -1,3 +1,11 @@
+export interface Personnel {
+	name: string;
+	specialty: string;
+	department: string;
+	description: string;
+	image: string;
+}
+
 interface Doctor {
 	id: number;
 	name: string;
@@ -13,50 +21,43 @@ interface Doctor {
 	education: Record<string, string>;
 }
 
-interface Location {
-	name: string;
-	city: string;
-	address: string;
-	phone: string;
-}
-
-const personnel: Doctor[] = [
+const personnel: Doctor = [
 	{
 		id: 1,
 		name: 'Dr. Ariel Amaru, MD, PhD',
-		specialty: 'ONCOHEMATOLOGY',
+		specialty: 'ONCOHEMATLOGÍA',
 		specialties: [
-			'Bone Marrow Transplant',
-			'Hematology and Oncohematology',
-			'Coagulation Disorders',
-			'Erythrocytosis and Anemia',
-			'Oncohematology',
-			'Bone Marrow Transplant',
-			'Molecular Biology',
-			'Genetic Sequencing',
-			'Oncological Research',
+			'Trasplante de Médula Ósea',
+			'Hematología y Oncohematología',
+			'Trastornos de coagulación',
+			'Eritrocitosis y Anemias',
+			'Oncohematología',
+			'Trasplante de Médula Ósea',
+			'Biología Molecular',
+			'Secuenciación Genética',
+			'Investigación Oncológica',
 		],
 		location: {
-			name: 'ONCOCLINIC ONCOLOGICAL CLINIC',
+			name: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
 			city: 'LA PAZ',
-			address: 'Obrajes, Avenida Costanerita, between streets 5 and 6, No. 71.',
+			address: 'Obrajes, Avenida Costanerita, entre calles 5 y 6, No. 71.',
 			phone: '22785566',
 		},
 		education: {
-			'Medical Surgeon':
-				'Medical Surgeon from the Universidad Mayor de San Andrés, Bolivia',
-			Hematologist: 'Oncohematologist from Hospital Papa Giovanni XXIII, Italy',
-			Doctorate:
-				'Ph.D. from the University of Milan-Bicocca (UNIMIB), Italy',
-			'Bone Marrow Transplant Specialist':
-				'Bone Marrow Transplant Specialty at the University of Chicago, USA',
+			'Medico Cirujano':
+				'Médico Cirujano de la Universidad Mayor de San Andrés, Bolivia',
+			Hematologo: 'Oncohematólogo del Hospital Papa Giovanni XXIII, Italia',
+			Doctorado:
+				'Doctorado en la Universidad de Milán-Bicocca (UNIMIB), Italia',
+			'Especilista de Transplantes':
+				'Especialidad en Trasplante de Médula Ósea en la Universidad de Chicago, EE. UU.',
 		},
-		languages: ['English', 'Spanish'],
+		languages: ['Ingles', 'Español'],
 		about: [
-			'Dr. Ariel Amaru is the Medical Director of Oncoclinic, responsible for the Bone Marrow Stem Cell Transplant program for oncohematological diseases such as leukemia, lymphoma, myeloma, or myelofibrosis, and for patients with bone marrow aplasia. He is an author of scientific journals and collaborates in research work.',
-			'Dr. Ariel Amaru graduated from the Faculty of Medicine of UMSA in La Paz, Bolivia, completed his Oncohematology and Bone Marrow Transplant specialty at the "Ospedale Papa Giovanni XXII" in Bergamo, Italy, and completed his Ph.D. at the University of Milan-Bicocca (UNIMIB) in Milan, Italy. He later completed postdoctoral studies at the University of Chicago in the United States.',
-			'In Bolivia, he was a postgraduate professor at UMSA in La Paz and UMSS in Cochabamba. In 2017, he initiated the Bone Marrow Transplant Program in Bolivia, and in 2023, he entered an accreditation program for the transplant unit.',
-			'In 2023, he established his Research Laboratory at Oncoclinic, performing high-tech techniques in Molecular Biology and Genetic Sequencing for research in oncological diseases.',
+			'El Dr. Ariel Amaru es el Director Médico de Oncoclinic, responsable del área de Trasplante de Células Madre para enfermedades oncohematológicas, como las leucemias, linfomas, mielomas o mielofibrosis, y para pacientes con aplasia de médula ósea.  Es autor de revistas científicas y colaborador en trabajos de investigación',
+			'El Dr. Ariel Amaru se graduó en la Facultad de Medicina de la UMSA de la ciudad de La Paz en Bolivia,  realizó sus estudios de especialidad de Oncohematologia y Trasplante de Médula Ósea en  el “Ospedale Papa Giovanni XXII” en Bérgamo, Italia y su estudio de Doctorado en la Universidad de Milano-Bicocca (UNIMIB) en Milán, Italia. Posteriormente realizo sus estudios Posdoctorado en la Universidad de Chicago en Estados Unidos.',
+			'En Bolivia fue Docente de PosGrado en las Universidad de la UMSA de la ciudad de La Paz  y UMSS de Cochabamba. En 2017 inicia el Programa de Trasplantes de Médula Ósea en Bolivia, en 2023 ingresa a un programa de acreditación de la unidad de trasplantes.',
+			'Desde el año 2023 estableció su Laboratorio de Investigación en Oncoclinic realizando técnicas de alta tecnología en Biología Molecular y Secuenciación Genética para la investigación en enfermedades oncológicas.',
 		],
 		image:
 			'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-SVIYB1uGFUUjRvdHB8uDzPDz4Kk3NB.png',
@@ -69,130 +70,128 @@ const personnel: Doctor[] = [
 	{
 		id: 2,
 		name: 'Dra. Jimena Yudid Bueno Limachi',
-		specialty: 'Oncological Surgery',
 		specialties: [
-			'Oncological Surgery',
-			'Specialist Doctor – Oncological Surgeon',
-			'General Surgeon',
+			'Cirugía Oncológica',
+			'Médico especialista – Cirujano Oncólogo',
+			'Cirujano general',
 		],
-		location: {
-			name: 'ONCOCLINIC ONCOLOGICAL CLINIC',
-			city: 'LA PAZ',
-			address: 'Obrajes, Avenida Costanerita, between streets 5 and 6, No. 71.',
-			phone: '22785566',
-		},
+		location: [
+			{
+				clinic: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
+				address: 'Obrajes, Avenida Costanerita, entre calles 5 y 6, No. 71.',
+				phone: '22785566',
+			},
+			{
+				clinic: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
+				address:
+					'Sucursal Sopocachi Av. 6 de agosto, Torre 2255 Mezanine (Frente al cine 6 de agosto)',
+				phone: '72026935',
+			},
+		],
 		education: {
-			'Medical Surgeon':
-				'From Universidad Mayor de San Andrés',
-			'Medical Residency in General Surgery at Hospital Obrero',
-			'Medical Residency in Subspecialty Oncological Surgery at HODE Oncológico – CNS',
+			'Médico Cirujano' :' de la Universidad Mayor de San Andrés',
+			'Residencia Médica Cirugía General en el Hospital Obrero',
+			'Residencia Médica Subespecialidad Cirugía Oncológica en HODE Oncológico – CNS',
 		},
-		languages: ['Spanish'],
-		about: [],
-		image:
-			'https://example.com/doctor-image.jpg', // Add image URL
-		hasVideo: false,
 	},
-
 	{
-		id: 3,
 		name: 'Dra. Erika Lourdes Patiño Constancio',
-		specialty: 'Head and Neck Oncological Surgery',
 		specialties: [
-			'Head and Neck Oncological Surgeon',
-			'General Surgeon',
-			'Head and Neck Surgery',
-			'Maxillectomies',
-			'Laryngectomies',
-			'Thyroidectomies',
-			'Mandiublectomies',
-			'Skin Tumors',
-			'Cytoreductive Surgery',
-			'Multidisciplinary Management',
+			'Cirujano Oncológico de cabeza y cuello',
+			'Médico especialista – Cirugía general',
+			'Cirujano general',
+			'Cirugía de Cabeza y Cuello',
+			'Maxilectomías',
+			'Laringectomías',
+			'Tiroidectomías',
+			'Mandibulectomías',
+			'Tumores en piel',
+			'Cirugía citorreductora',
+			'Manejo multidisciplinario',
 		],
-		location: {
-			name: 'ONCOCLINIC ONCOLOGICAL CLINIC',
-			city: 'LA PAZ',
-			address: 'Obrajes, Avenida Costanerita, between streets 5 and 6, No. 71.',
-			phone: '22785566',
-		},
+		location: [
+			{
+				clinic: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
+				address: 'Obrajes, Avenida Costanerita, entre calles 5 y 6, No. 71.',
+				phone: '22785566',
+			},
+			{
+				clinic: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
+				address:
+					'Sucursal Sopocachi Av. 6 de agosto, Torre 2255 Mezanine (Frente al cine 6 de agosto)',
+				phone: '72026935',
+			},
+		],
 		education: [
-			'Professional from Universidad Mayor de San Andrés',
-			'Medical Residency in General Surgery at the Hospital Militar Central',
-			'Medical Residency in Subspecialty Head and Neck Oncological Surgeon at INEN, Lima – Peru',
+			'Profesional de la Universidad Mayor de San Andrés',
+			'Residencia Médica Cirugía General en el Hospital Militar Central',
+			'Residencia Médica Subespecialidad Cirujano Oncológico de cabeza y cuello en INEN, Lima – Perú',
 		],
-		languages: ['Spanish'],
-		about: [],
-		image:
-			'https://example.com/doctor-image.jpg', // Add image URL
-		hasVideo: false,
 	},
-
 	{
-		id: 4,
 		name: 'Dr. Marcio Denis López Ramirez',
-		specialty: 'Clinical Oncologist',
 		specialties: [
-			'Clinical Oncologist',
-			'Specialty in Immunotherapy Treatment',
-			'Specialty in Biological Therapy and Chemotherapy',
-			'Clinical Oncology',
-			'Chemotherapy',
-			'Immunotherapy',
-			'Biological Therapy',
-			'Molecular Biology Applied to Clinical Oncology',
-			'Tumor DNA Sequencing',
-			'Multidisciplinary Management',
+			'Oncólogo Clínico',
+			'Especialidad en Tratamiento en Inmunoterapia',
+			'Especialidad en Terapia Biológica y Quimioterapia',
+			'Oncología Clínica',
+			'Quimioterapia',
+			'Inmunoterapia',
+			'Terapia Biológica',
+			'Biología Molecular aplicada a Oncología Clínica',
+			'Secuenciación del ADN tumoral',
+			'Manejo multidisciplinario',
 		],
-		location: {
-			name: 'ONCOCLINIC ONCOLOGICAL CLINIC',
-			city: 'LA PAZ',
-			address: 'Obrajes, Avenida Costanerita, between streets 5 and 6, No. 71.',
-			phone: '22785566',
-		},
+		location: [
+			{
+				clinic: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
+				address: 'Obrajes, Avenida Costanerita, entre calles 5 y 6, No. 71.',
+				phone: '22785566',
+			},
+			{
+				clinic: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
+				address:
+					'Sucursal Sopocachi Av. 6 de agosto, Torre 2255 Mezanine (Frente al cine 6 de agosto)',
+				phone: '72026935',
+			},
+		],
 		education: [
-			'Molecular Biology Applied to Clinical Oncology at the University of Liège, Belgium',
-			'Doctorate in Medicine at the Latin American Medical College',
-			'General Medicine at the Faculty of Medical Sciences Faustino Pérez Hernández',
-			'Oncologist, Chemotherapy, Immunotherapy at the Faculty of Medical Sciences Faustino Pérez Hernández',
-			'Cancer Control, Pain Management, and Palliative Care at the Camilo Cienfuegos University Hospital',
+			'Biología Molecular aplicada a la Oncología Clínica en la Universidad de Liège, Bélgica',
+			'Doctorado en Medicina en el Colegio Médico Latinoamericano',
+			'Medicina General Integral de la Facultad de Ciencias Médicas Faustino Pérez Hernández',
+			'Médico Oncólogo, Quimioterapia, Inmunoterapia en la Facultad de Ciencias Médicas Faustino Pérez Hernández',
+			'Control de Cáncer, Manejo del dolor y Cuidados Paliativos en el Hospital Universitario Camilo Cienfuegos',
 		],
-		languages: ['Spanish', 'English'],
-		about: [],
-		image:
-			'https://example.com/doctor-image.jpg', // Add image URL
-		hasVideo: false,
 	},
-
 	{
-		id: 5,
 		name: 'Dr. Daniel Eduardo Mercado Rodrigo',
-		specialty: 'Orthopedic Surgeon',
 		specialties: [
-			'Medical Surgeon',
-			'Specialty in Orthopedics and Traumatology',
-			'Orthopedic Oncology',
-			'Orthopedic Oncology',
-			'Skeletal Tumors',
-			'Reconstructive Surgery with Modular Prostheses',
+			'Médico Cirujano',
+			'Especialidad en Ortopedia y Traumatología',
+			'Especialidad en Ortopedia Oncológica',
+			'Ortopedia Oncológica',
+			'Tumores Muscoesqueléticos',
+			'Cirugía reconstructiva con prótesis modulares',
 		],
-		location: {
-			name: 'ONCOCLINIC ONCOLOGICAL CLINIC',
-			city: 'LA PAZ',
-			address: 'Obrajes, Avenida Costanerita, between streets 5 and 6, No. 71.',
-			phone: '22785566',
-		},
+		location: [
+			{
+				clinic: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
+				address: 'Obrajes, Avenida Costanerita, entre calles 5 y 6, No. 71.',
+				phone: '22785566',
+			},
+			{
+				clinic: 'ONCOCLINIC CLÍNICA ONCOLÓGICA',
+				address:
+					'Sucursal Sopocachi Av. 6 de agosto, Torre 2255 Mezanine (Frente al cine 6 de agosto)',
+				phone: '72026935',
+			},
+		],
 		education: [
-			'Medical Surgeon from Universidad Nuestra Señora de La Paz',
-			'Specialist in Orthopedics and Traumatology',
-			'Advanced Specialty in Orthopedic Oncology at INR-Unam Mexico',
-			'Diploma in Bone Infections and Pseudoarthrosis at INR-Unam Mexico',
+			'Médico Cirujano de la Universidad Nuestra Señora de La Paz',
+			'Especialista en Ortopedia y Traumatología',
+			'Alta especialidad en ortopedia oncológica INR-Unam México',
+			'Diplomado en Infecciones óseas y pseudoartrosis INR-Unam México',
 		],
-		languages: ['Spanish', 'English'],
-		about: [],
-		image:
-			'https://example.com/doctor-image.jpg', // Add image URL
-		hasVideo: false,
 	},
 ];
 
